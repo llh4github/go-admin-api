e1b0093e702059f5f43b34268bfc82d9443a49a5
llh <forever2y_y@163.com>
2020-09-11 11:59:57 +0800

    delete: 无用代码

--- service/init.go
+++ service/init.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"github.com/llh4github/go-admin-api/global"
 	"github.com/llh4github/go-admin-api/utils"
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
@@ -24,17 +23,4 @@ const (
 
 // Base 基础服务
 type Base struct {
-}
-
-// InitService for wire tool
-func InitService() *Base {
-	db = global.MyDB
-	log = global.MyLog
-
-	w, err := utils.CreateWorker(3, 3)
-	if err != nil {
-		panic("雪花算法初始化错误")
-	}
-	idWorker = w
-	return nil
 }
